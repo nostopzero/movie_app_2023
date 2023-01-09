49fdb713335c9803055a421a60e443841b4bdb2e
노서영 <hello.syeong@partner.sk.com>
2023-01-09 16:32:32 +0900

    03 Learning React Basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+function Movie({fav}) {
+  return <h3>I love {fav}.</h3>;
+}
+
 function App() {
-  return <div>Helllllllo!!!!!</div>;
+  return (
+    <div>
+      <h1>Helllllllo React</h1>
+      <Movie fav="avatar" />
+      <Movie fav="criminal" />
+      <Movie fav="saw" />
+    </div>
+  );
 }
 
 export default App;
